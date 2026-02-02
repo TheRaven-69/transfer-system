3389e3ab38705a1570ea9427e51679b040596447
TheRaven-69 <nagibaror64@gmail.com>
2026-02-02 18:51:22 +0100

    Add database tests for models and implement DB engine

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from models import Base
+from database import engine, get_db
 
-
-app =FastAPI(title="Transfer System API")
+app = FastAPI(title="Transfer System API")
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.get("/db-check")
+def db_check(db: Session = Depends(get_db)):
+    return {"db": "ok"}
 
 
