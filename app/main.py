b9c9a6313017c909a5382794c553af359f85330f
TheRaven-69 <nagibaror64@gmail.com>
2026-02-24 00:05:37 +0100

    add minimal landing page and improve README

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse, HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, JSONResponse
 
 from app.api.routes import router
 from app.db.models import Base
